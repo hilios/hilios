d094053d26b75fa16b891bb264bdfe016206e73e
Edson Hilios <edson.hilios@telefonicabeta.com>
2013-05-27 14:10:46 -0300

    Fix typo on exception

--- config/initializers/tumblr.rb
+++ config/initializers/tumblr.rb
@@ -5,7 +5,7 @@ tumblr_config = File.expand_path('~/.tumblr')
 if File.exists? tumblr_config
   tumblr_config = YAML::load_file(tumblr_config)
 else
-  raise "Tumblr API configuration file was not found at #{TUMBLR_KEY}"
+  raise "Tumblr API configuration file was not found at #{tumblr_config}"
 end
 
 Tumblr.configure do |config|
