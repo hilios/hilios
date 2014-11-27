7c8cd277f35e95b6541afd6f53ed35c3e6f14fec
Edson Hilios <edson.hilios@telefonicabeta.com>
2014-11-27 18:33:47 -0200

    Add the api key to the request

--- src/hilios.com.br/tumblr/tumblr.go
+++ src/hilios.com.br/tumblr/tumblr.go
@@ -2,7 +2,8 @@ package tumblr
 
 import (
 	"fmt"
-	// "github.com/golang/oauth2"
+	"log"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"regexp"
@@ -10,36 +11,33 @@ import (
 
 var path = regexp.MustCompile("^/tumblr(/.*)$")
 
-type tumblrRequest struct {
-	ApiKey string
-	Method string
-}
+const ApiKey string = ""
 
 func ProxyHandler(w http.ResponseWriter, r *http.Request) {
 	// Parse the API url
 	p := path.FindStringSubmatch(r.URL.Path)
-	url := "http://api.tumblr.com" + p[1]
-
+	url := fmt.Sprintf("http://api.tumblr.com%s", p[1])
 	// Creates a new request
 	t, _ := http.NewRequest(r.Method, url, nil)
-
-	t.Header.Add("api_key", "")
+	// Inject the API key to the url
+	t.URL.RawQuery = fmt.Sprintf("api_key=%s&%s", ApiKey, t.URL.RawQuery)
+	// Print a log
+	log.Println(t.URL.String());
 	// Do the request
 	c := http.Client{}
 	res, err := c.Do(t)
-
+	// Complain if request couldn't be made
 	if err != nil {
-		http.Error(w, err.Error(), 403)
+		http.Error(w, err.Error(), 400)
 	}
-
+	// Close the body eventually
 	defer res.Body.Close()
-
+	// Read the body
 	body, err := ioutil.ReadAll(res.Body)
-
+	// Complain if cannot read response body
 	if err != nil {
 		http.Error(w, err.Error(), 403)
 	}
-
-	fmt.Fprint(w, string(body))
-	// io.WriteString(w, string(body))
+	// Dump the body to Request
+	io.WriteString(w, string(body))
 }
