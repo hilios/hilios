2d29e10061ef014bff2475fe28da31646bb8ad9c
Edson Hilios <edson.hilios@telefonicabeta.com>
2014-12-04 10:31:16 -0200

    Improve gulp dispatcher to let main process handle signal directly

--- server/gulp.go
+++ server/gulp.go
@@ -1,35 +1,20 @@
 package server
 
 import (
+	"log"
 	"os"
 	"os/exec"
-	"os/signal"
 )
 
+// Start the gulp command concurrently to main thread.
 func gulp(cwd string) {
-	p := os.Getpid()
+	log.Println("Staring Gulp...")
 	// Register Gulp Cmd and bind stdout and stderr
 	cmd := exec.Command("gulp", "--cwd", cwd)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stdout
-	// Capture Ctrl+C and stop CPU profiler
-	k := make(chan os.Signal, 1)
-	signal.Notify(k, os.Interrupt, os.Kill)
 	// Start the command
 	if err := cmd.Start(); err != nil {
 		panic(err)
 	}
-	// Wait for the kill signal in another thread
-	go func(kill chan os.Signal) {
-		for {
-			// Release the signal after the function
-			defer signal.Stop(kill)
-			// Wait for it
-			<-kill
-			cmd.Process.Signal(os.Kill)
-			// Kill main thread as well
-			main, _ := os.FindProcess(p)
-			main.Signal(os.Kill)
-		}
-	}(k)
 }
