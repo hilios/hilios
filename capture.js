2c2b2fb124e2088f79eb301905eea07f952d7ecb
Edson Hilios <edson.hilios@telefonicabeta.com>
2015-02-04 15:51:17 -0200

    Create a little ss server

---
+++ capture.js
@@ -0,0 +1,142 @@
+// https://github.com/superbrothers/capturejs/blob/master/lib/capturejs.js
+"use strict";
+
+var phantom = require("phantom");
+
+// Pick a random int between two ranges.
+function getRandomPort(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+module.exports.capture = function (option, callback) {
+    callback = callback || function () { return undefined; };
+
+    var args = [],
+        phantomHandler,
+        port = getRandomPort(40000, 60000);
+
+    // Handler for Phantom.js
+    phantomHandler = function (ph) {
+        ph.createPage(function (page) {
+            var timer,
+                matches;
+
+            // viewportSize option
+            if (option.viewportsize) {
+                matches = option.viewportsize.match(/^(\d+)x(\d+)$/);
+                if (matches !== null) {
+                    page.set("viewportSize", {width: matches[1], height: matches[2]});
+                }
+            }
+
+            // clipRect option
+            if (option.cliprect) {
+                matches = option.cliprect.match(/^(\d+)x(\d+)x(\d+)x(\d+)$/);
+                if (matches !== null) {
+                    page.set("clipRect", {top: matches[1], left: matches[2],
+                        width: matches[3], height: matches[4]});
+                }
+            }
+
+            // UserAgent option
+            if (option["user-agent"]) {
+                page.set("settings.userAgent", option["user-agent"]);
+            }
+
+            // zoomFactor option
+            if (option.zoomfactor) {
+                page.set("zoomFactor", parseFloat(option.zoomfactor));
+            }
+
+            // HTTP Timeout option
+            if (option.timeout) {
+                timer = setTimeout(function () {
+                    var err = "Error: " + option.uri + " is timeout";
+                    ph.exit();
+                    process.nextTick(function () {
+                        callback(err);
+                    });
+                }, +option.timeout);
+            }
+
+            // Open web page
+            page.open(option.uri, function (status) {
+                if (status === "fail") {
+                    var err = "Error: " + option.uri + "failed";
+                    ph.exit();
+                    process.nextTick(function () {
+                        callback(err);
+                    });
+                }
+
+                if (timer !== undefined) {
+                    clearTimeout(timer);
+                }
+
+                // Injecting external script code
+                if (option["javascript-file"]) {
+                    page.injectJs(option["javascript-file"]);
+                }
+
+                /**
+                * pass parameters into the webpage function.
+                *
+                * http://code.google.com/p/phantomjs/issues/detail?id=132
+                * from comment #43.
+                */
+                page.evaluate((function () {
+                    var func = function (selector) {
+                        document.body.bgColor = "white";
+                        if (selector === undefined) {
+                            return null;
+                        }
+                        var elem = document.querySelector(selector);
+                        return (elem !== null) ? elem.getBoundingClientRect() : null;
+                    };
+                    return "function() { return (" + func.toString() + ").apply(this, " + JSON.stringify([option.selector]) + ");}";
+                }()), function (rect) {
+                    if (rect !== null) {
+                        page.set("clipRect", rect);
+                    }
+                    // Set a timeout to give the page a chance to render
+                    setTimeout(function () {
+                        page.render(option.output, function () {
+                            ph.exit();
+                            process.nextTick(callback);
+                        });
+                    }, option.renderdelay || 250);
+
+
+                });
+            });
+        });
+    };
+
+    // Setup arguments for Phantom.js
+    ["cookies-file", "ignore-ssl-errors", "ssl-protocol"].forEach(function (key) {
+        if (option[key]) {
+            args.push(["--", key, "=", option[key]].join(""));
+        }
+    });
+    args.push(phantomHandler);
+
+
+    // Set the handler in case we grab an in-use port.
+    process.on("uncaughtException", function (err) {
+        if (err.errno === "EADDRINUSE") {
+            port = getRandomPort(40000, 60000);
+            args.unshift({"port": port});
+            phantom.create.apply(phantom, args);
+        } else {
+            console.log(err);
+            process.exit(1);
+        }
+    });
+
+    // Pick a random port for phantom to use on start up
+
+    args.unshift({"port": port});
+    phantom.create.apply(phantom, args);
+
+};
+
---
+++ main.js
@@ -0,0 +1,63 @@
+var capturejs = require('./capture');
+var restify = require('restify');
+var crypto = require('crypto');
+var path = require('path')
+var fs = require('fs');
+
+var server = restify.createServer({
+  name: 'extras',
+  version: '1.0.0'
+});
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.gzipResponse());
+server.use(restify.queryParser());
+
+function readFile(path, r) {
+  try {
+    f = fs.readFileSync(path);
+    r.write(f);
+    r.end();
+  } catch(e) {
+    r.writeHead(500);
+    r.end('');
+  }
+}
+
+function capture(url, path, r) {
+  capturejs.capture({
+    uri: url,
+    output: path,
+    cliprect: '0x0x1280x720',
+    viewportsize: '1280x769'
+  }, function(err) {
+    if (err) {
+      r.writeHead(400);
+      r.end('');
+    } else {
+      readFile(path, r);
+    }
+  });
+}
+
+server.get('/p', function (req, res, next) {
+  var url = req.query['q'];
+
+  if (url === undefined) {
+    res.writeHead(400);
+    res.end('');
+    return next();
+  }
+  // Generate the filename to cache
+  var cache = crypto.createHash('md5').update(url).digest('hex');
+  var filepath = path.join(process.cwd(), 'tmp', cache) + '.png';
+
+  if (!fs.existsSync(filepath)) {
+    capture(url, filepath, res);
+  } else {
+    readFile(filepath, res);
+  }
+});
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
